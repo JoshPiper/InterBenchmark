43228f917b0b76b90078aa37e7bc8f4e6c29d927
Joshua Piper <32164094+JoshPiper@users.noreply.github.com>
2021-05-27 13:46:20 +0100

    fix: Fixes crash caused by a shadowing a.

--- lua/internet_benchmarks/trials/for_loops.lua
+++ lua/internet_benchmarks/trials/for_loops.lua
@@ -1,12 +1,12 @@
-local a = {}
+local tab = {}
 for i = 1, 100 do
-	a[i] = i
+	tab[i] = i
 end
 
-a.n = 100
-a[0] = 100
+tab.n = 100
+tab[0] = 100
 
-local length = #a
+local length = #tab
 local nxt = next
 
 local function jit_pairs(t)
@@ -14,50 +14,50 @@ local function jit_pairs(t)
 end
 
 local function a()
-	for k, v in pairs(a) do
+	for k, v in pairs(tab) do
 		local x = v
 	end
 end
 
 local function b()
-	for k, v in jit_pairs(a) do
+	for k, v in jit_pairs(tab) do
 		local x = v
 	end
 end
 
 local function c()
-	for k, v in ipairs(a) do
+	for k, v in ipairs(tab) do
 		local x = v
 	end
 end
 
 local function d()
 	for i = 1, 100 do
-		local x = a[i]
+		local x = tab[i]
 	end
 end
 
 local function e()
-	for i = 1, #a do
-		local x = a[i]
+	for i = 1, #tab do
+		local x = tab[i]
 	end
 end
 
 local function f()
 	for i = 1, length do
-		local x = a[i]
+		local x = tab[i]
 	end
 end
 
 local function g()
-	for i = 1, a.n do
-		local x = a[i]
+	for i = 1, tab.n do
+		local x = tab[i]
 	end
 end
 
 local function h()
-	for i = 1, a[0] do
-		local x = a[i]
+	for i = 1, tab[0] do
+		local x = tab[i]
 	end
 end
 
@@ -74,9 +74,9 @@ return {
 		{title = "jit pairs", func = b},
 		{title = "ipairs", func = c},
 		{title = "for i fixed max", func = d},
-		{title = "for i #a", func = e},
+		{title = "for i #tab", func = e},
 		{title = "for i length", func = f},
-		{title = "for i a.n", func = g},
-		{title = "for i a[0]", func = h}
+		{title = "for i tab.n", func = g},
+		{title = "for i tab[0]", func = h}
 	}
 }

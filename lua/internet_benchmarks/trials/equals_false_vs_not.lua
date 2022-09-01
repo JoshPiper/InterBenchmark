6318cbc84395ca632c16ac0e5e6e3f70507dadbe
JoshPiper <32164094+JoshPiper@users.noreply.github.com>
2022-09-01 02:42:51 +0100

    feat: Add not val benchmark.

---
+++ lua/internet_benchmarks/trials/equals_false_vs_not.lua
@@ -0,0 +1,27 @@
+local testVal = false
+local setVal
+
+function a()
+	if testVal == false then
+		setVal = 1
+	end
+end
+
+function b()
+	if not testVal then
+		setVal = 1
+	end
+end
+
+return {
+	meta = {
+		title = "val == false vs not val",
+		order = 1000,
+		runs = 500,
+		iterations = 100000
+	},
+	functions = {
+		{title = "== false", func = a},
+		{title = "not val", func = b},
+	}
+}

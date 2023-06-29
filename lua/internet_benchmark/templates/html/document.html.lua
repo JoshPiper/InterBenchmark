dfdda67d65238ca6eddf126f92988fc21dcc94e6
JoshPiper <32164094+JoshPiper@users.noreply.github.com>
2023-06-29 18:34:39 +0100

    feat: Add canvas.

--- lua/internet_benchmark/templates/html/document.html.lua
+++ lua/internet_benchmark/templates/html/document.html.lua
@@ -8,6 +8,10 @@
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.6.0/styles/default.min.css">
 		<link rel="stylesheet" href="style.css">
+		<script src="https://cdn.canvasjs.com/ga/canvasjs.min.js"></script>
+		<script>
+			let charts = new Map()
+		</script>
 	</head>
 
 	<body>

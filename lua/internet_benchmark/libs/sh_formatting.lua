5fb126f049ce00ccdd2eb9b4118273a24f363ea2
JoshPiper <32164094+JoshPiper@users.noreply.github.com>
2023-06-29 18:54:04 +0100

    feat: Bring over some old functions.

--- lua/internet_benchmark/libs/sh_formatting.lua
+++ lua/internet_benchmark/libs/sh_formatting.lua
@@ -32,6 +32,15 @@ FORMAT.AllowedPrefixes = {
 	standard = {-24, -21, -18, -15, -12, -9, -6, -3, 0, 3, 6, 9, 12, 15, 18, 21}
 }
 
+FORMAT.DontLookup = {
+	"GCompute",
+	"GLib",
+	"EMVU",
+	"Photon",
+	"package.loaded",
+}
+FORMAT.Lookedup = {}
+
 function FORMAT:GetAllowedPrefixes(name)
 	if isstring(name) then
 		return self.AllowedPrefixes[name] or self.AllowedPrefixes.standard
@@ -119,4 +128,126 @@ function FORMAT.Title(word)
 	word = string.gsub(word, " %w", string.upper)
 	word = string.gsub(word, "^%w", string.upper)
 	return word
+end
+
+function FORMAT.Source(path, start, stop)
+	local data = file.Read(path, "LUA")
+	data = string.Explode("\n", data)
+	return table.concat(data, "\n", start, stop)
+end
+
+function FORMAT:Function(fn)
+	local info = debug.getinfo(fn, "flLnSu")
+	if info.what == "Lua" and info.short_src:find("/lua/") then
+		local body = self:ReadSource(path, info.linedefined, info.lastlinedefined)
+		body = body:Trim()
+
+		-- body = string.Explode("\n", body)
+
+		-- local first = body[1]
+		-- if first then
+		-- 	-- Strip the func prefix from anonymous functions.
+		-- 	first = first:gsub("^[%s%w]*function%(", "")
+		-- 	-- Ditto for named functions.
+		-- 	first = first:gsub("^[%s%w]*function [%w_]+%(", "")
+		-- 	if first:StartWith(")") then
+		-- 		first = first:sub(1)
+		-- 	end
+		-- 	body[1] = first
+		-- end
+
+		-- local last = body[#body]
+		-- if last then
+		-- 	last = last:gsub("end[%w%s,]-$", "")
+		-- 	body[#body] = last
+		-- end
+
+		-- Finally, make all functions anonymous.
+		-- return "function(" .. table.concat(body, "\n")
+
+
+		return body
+	end
+
+	return self:Lookup(fn)
+end
+
+function FORMAT:Lookup(var, inTable, route, seen)
+	if self.Lookedup[var] then
+		return self.Lookedup[var]
+	end
+
+	if not seen then seen = {[_G] = true} end
+	if not route then route = {} end
+	if not inTable then inTable = _G end
+
+	local cur = table.concat(route, ".")
+	for _, bl in ipairs(self.LookupBlacklist) do
+		if cur:StartWith(bl) then
+			return false
+		end
+	end
+
+	local toDo = {}
+	for k, v in pairs(inTable) do
+		if v == var then
+			table.insert(route, k)
+			route = table.concat(route, ".")
+			self.LookupCache[var] = route
+			return route
+		end
+
+		if istable(v) and not seen[v] then
+			toDo[k] = v
+		end
+	end
+
+	for k, v in pairs(toDo) do
+		-- Prevent infinate loop.
+		seen[v] = true
+		table.insert(route, k)
+
+		local found = self:Lookup(var, v, route, seen)
+		if found then
+			return found
+		end
+
+		seen[v] = nil
+		table.remove(route)
+	end
+
+	return false
+end
+
+function FORMAT:Local(var, excludeGlobals)
+	local val
+	if isfunction(var) then
+		if not val and not excludeGlobals then
+			val = self:LookupGlobal(var)
+		end
+
+		if not val then
+			local src = self:ReadFunction(var)
+			if src then
+				val = {"raw", src}
+			end
+		end
+
+		if not val then
+			val = string.format("function() end -- Unknown Function %p", var)
+		end
+	elseif isstring(v) then
+		val = string.format("%q", var)
+	elseif isnumber(v) then
+		val = var
+	elseif IsColor(v) then
+		val = Format("Color(%s, %s, %s, %s)", v.r, v.g, v.b, v.a)
+	else
+		-- This will need to be expanded, but for now it should work.
+		val = tostring(var)
+	end
+
+	if val then
+		return val
+	end
 end
